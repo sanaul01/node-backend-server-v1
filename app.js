b9fb789491e404f0b9a6d6e2849452d70e24265d
sanaul01 <mdsanaulislam154@gmail.com>
2023-09-16 19:58:36 +0600

    added schema

--- app.js
+++ app.js
@@ -1,13 +1,92 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require('mongoose');
 
-
+// Midilware
 app.use(express.json());
 app.use(cors());
+
+
+// schema design 
+const productSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required:[true, "please provide a naem for this product"],
+        trim: true,  //remove speech,
+        unique:[true, "Name must be at least 3 characters"],
+        maxLength:[100, "Name is too large"]
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true,
+        min: [0, "price can't be negative"]
+    },
+    unit:{
+        type: String,
+        required: true,
+        enum:{
+            values: ["kg", "litre", "pcs"],
+            message: "unit value can't be {VALUE} must be kg/litre/pcs"
+        }
+    },
+    quantity:{
+        type: Number,
+        required: true,
+        min: [0, "quantity can't be negative"],
+        validate:{
+            validator:(value)=>{
+                const isInteger = Number.isInteger(value);
+                if(isInteger){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        },
+        message: "Quantity must be an integer"
+    },
+    status:{
+        type: String,
+        required: true,
+        enum:{
+            values:["in-stock", "out-of-stock", "discontinued"],
+            message: "statuse can't be {VALUE}"
+        }
+    },
+    // taking as a reference 
+    // supplier:{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "Supplier"
+    // },
+    // // embeded model 
+    // categories:[{
+    //     name:{
+    //         type: String,
+    //         required: true
+    //     },
+    //     _id: mongoose.Schema.Types.ObjectId
+    // }]
+},{
+    timestamps: true
+});
+
+// schema model 
+const Product = mongoose.model("Product", productSchema)
 
 app.get('/', (req, res)=>{
     res.send("welcome to my server at node mongodb backend")
 });
+
+app.post('/api/v1/product', (req, res, next)=>{
+   const product = new Product(req.body);
+
+
+   product.save();
+}); 
 
 module.exports = app;
